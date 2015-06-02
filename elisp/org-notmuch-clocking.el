f907a951d7d281d7beabca94198a6cdd150da52b
seamus tuohy <s2e@seamustuohy.com>
2015-06-02 08:21:49 -0400

    moved all config files into a single org file.

--- elisp/org-notmuch-clocking.el
+++ elisp/org-notmuch-clocking.el
@@ -1,9 +1,25 @@
-;; org-notmuch-clocking
+;; org-notmuch-clocking --- A library that automates e-mail clocking when using notmuch.
 ;; Copyright (C) 2015 seamus tuohy
 ;; Keywords: org time notmuch clocking
 ;; Author: seamus tuohy s2e@seamustuohy.com
 ;; Maintainer: seamus tuohy s2e@seamustuohy.com
 ;; Created: 2015 May 27
+
+;; When sending an response to an e-mail add the email clocking hooks
+
+;; (add-hook 'notmuch-mua-send-hook
+;;           (lambda ()
+;;             (add-hook 'focus-in-hook 'org-notmuch-clocking-email-clock-in nil 'make-it-local)
+;;             (add-hook 'focus-out-hook 'org-notmuch-clocking-email-clock-out nil 'make-it-local)))
+
+;; When opening an email add the email clocking hooks
+;; (add-hook 'notmuch-show-hook
+;;           (lambda ()
+;;             (add-hook 'focus-in-hook 'org-notmuch-clocking-email-clock-in nil 'make-it-local)
+;;             (add-hook 'focus-out-hook 'org-notmuch-clocking-email-clock-out nil 'make-it-local)))
+
+; (add-hook 'notmuch-show-hook 'org-notmuch-clocking)
+
 
 (require 'notmuch)
 (require 'org)
@@ -14,73 +30,58 @@
   "Settings for notmuch emails time clocking in org-mode."
   :group 'org)
 
-(defcustom
+(defcustom org-notmuch-clocking-file nil
   "The file to use to track emails."
   :type 'file
-  :group 'org-notmuch-clocking
-  org-notmuch-clocking-file "~/.org/email_clocking")
+  :group 'org-notmuch-clocking)
 
-;; Declare external functions and variables
-(declare-function notmuch-mua-send-hook "notmuch")
-(declare-function notmuch-show-hook "notmuch")
-(declare-function notmuch-show-get-message-id "notmuch")
-(declare-function notmuch-show-get-from "notmuch")
+(defun org-notmuch-clocking-email-clock ()
+  (interactive)
+  (let ((message_id (notmuch-show-get-message-id))
+        (if (condition-case nil
+                (string-equal message_id (org-entry-get org-clock-marker "ID"))
+              (error nil))
+            (org-notmuch-clocking-email-clock-out message_id)
+          (org-notmuch-clocking-email-clock-in)))))
 
-(defun org-notmuch-clocking-email-clock-out (message_id subject from)
+(defun org-notmuch-clocking-email-clock-out ()
   "Clock out of a the current email. Only clocks out if the current e-mail is the active clocking task."
-  (interactive (list (notmuch-show-get-message-id) (notmuch-show-get-subject) (notmuch-show-get-from)))
+  (interactive)
   (if (org-clocking-p)
-      (let ((location (org-id-find-in-file message_id org-notmuch-clocking-file))
-            (clocked-task (org-clock-marker)))
-        (if (org-notmuch-clocking-email-is-clocked-task)
-            (org-with-point-at location
-              (org-clock-out)))))
-  (message message_id) ;;TODO FIX STUB CODE HERE
-  (message from)) ;;TODO FIX STUB CODE HERE
+      (let* ((message_id (notmuch-show-get-message-id))
+             (email_loc (org-id-find-id-in-file message_id org-notmuch-clocking-file))
+             (email_mrkr (make-marker)))
+        (set-marker email_mrkr (cdr email_loc) (get-file-buffer (car email_loc)))
+        (if (string-equal message_id (org-entry-get org-clock-marker "ID"))
+            (org-with-point-at email_mrkr
+              (org-clock-out))))))
 
-(defun org-notmuch-clocking-email-clock-in (message_id subject from)
+(defun org-notmuch-clocking-email-clock-in ()
   "Clock in to the header for the current email. If the header does not exist, this function will create it first."
-   (interactive (list (notmuch-show-get-message-id) (notmuch-show-get-subject) (notmuch-show-get-from)))
-   (let (location (org-id-find-in-file message_id org-notmuch-clocking-file)))
-   (if not location
-     ((org-notmuch-clocking-add-email message_id subject from)
-      (org-notmuch-clocking-email-clock-in message_id subject from))
-     (org-with-point-at location
-       (org-clock-in))))
+  (interactive)
+  (message "clocking message in")
+  (if (org-id-find-id-in-file (notmuch-show-get-message-id) org-notmuch-clocking-file)
+      (let* ((email_loc (org-id-find-id-in-file (notmuch-show-get-message-id) org-notmuch-clocking-file))
+             (email_mrkr (make-marker)))
+        (set-marker email_mrkr (cdr email_loc) (get-file-buffer (car email_loc)))
+        (org-with-point-at email_mrkr
+          (org-clock-in)))
+    (org-notmuch-clocking-add-email)))
 
-(defun org-notmuch-clocking-add-email (message_id subject from)
+(defun org-notmuch-clocking-add-email ()
   "This function will create a header at the end of the current org-notmuch-clocking file"
-;(org-insert-heading &optional ARG INVISIBLE-OK)
-;; ;;If point is at the beginning of a heading or a list item, insert a new heading or a new item above the current one.  If point is at the beginning of a normal line, turn the line into a heading.
-; (org-insert-property-drawer)
-; (org-insert-drawer &optional ARG DRAWER)
+  (message "adding new email entry")
+  (let ((org-capture-entry '("W" "org-notmuch-clocking-email" entry
+                             (file org-notmuch-clocking-file)
+                             "" :clock-in t :clock-keep t))
+        (message_id (notmuch-show-get-message-id))
+        (subject (notmuch-show-get-subject))
+        (from (notmuch-show-get-from)))
+    (org-capture nil "W")
+    (org-insert-link nil (concat "notmuch:" message_id) (concat subject " From: " from))
+    (org-insert-property-drawer)
+    (org-entry-put nil "FROM" from)
+    (org-entry-put nil "ID" message_id)
+    (org-capture-finalize)))
 
-
-  ;
-  )
-
-(defun org-notmuch-clocking-email-is-clocked-task (email_mrkr clocked_loc)
-  (let ((clocked_mrkr make-marker))
-    (set-marker clocked_mrkr (cdr clocked_loc) (car clocked_loc))
-    (if (eq (
-
-             )
-
-
-    )
-;;When sending an response to an e-mail add the email clocking hooks
-(add-hook 'notmuch-mua-send-hook
-          (lambda ()
-            (add-hook 'focus-in-hook 'org-notmuch-clocking-email-clock-in nil 'make-it-local)
-            (add-hook 'focus-out-hook 'org-notmuch-clocking-email-clock-out nil 'make-it-local)))
-
-;;When opening an email add the email clocking hooks
-(add-hook 'notmuch-show-hook
-          (lambda ()
-            (add-hook 'focus-in-hook 'org-notmuch-clocking-email-clock-in nil 'make-it-local)
-            (add-hook 'focus-out-hook 'org-notmuch-clocking-email-clock-out nil 'make-it-local)))
-
-(set-marker MARKER POSITION &optional BUFFER)
-(provide org-notmuch-clocking)
-
-;;; XXX.el ends here
+(provide 'org-notmuch-clocking)
