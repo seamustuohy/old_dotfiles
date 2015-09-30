b70a4c0e68618888b99528541b84e5e02cab7dc3
seamus tuohy <s2e@seamustuohy.com>
2015-09-30 04:24:46 -0400

    moved files

---
+++ bin/elisp/org-regex.el
@@ -0,0 +1,14 @@
+;; org-regex.el
+;; Copyright (C) 2015 seamus tuohy
+;; Keywords: regex org-mode
+;; Author: seamus tuohy s2e@seamustuohy.com
+;; Maintainer: seamus tuohy s2e@seamustuohy.com
+;; Created: 2015 Aug 29
+
+
+
+
+
+(provide   'orx)
+
+;;; org-regex.el ends here
